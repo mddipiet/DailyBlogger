dd8a01471920194b9639ae8de8dcb01916576252
mddipiet <68350023+mddipiet@users.noreply.github.com>
2020-10-01 05:06:03 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DailyBlogger2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
